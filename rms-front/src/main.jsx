8ba2ce13500dfae8141e54fddf70c07df2265b2a
bachriotmane <bachriotmane@gmail.com>
2024-01-26 00:17:47 +0100

    New Commit

--- rms-front/src/main.jsx
+++ rms-front/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import AppClient from './pages/Client-pages/ClientApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppClient />
+    <App />
   </React.StrictMode>,
 )
