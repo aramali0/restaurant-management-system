f934be002d0f0ce05f7aaaba5121f2df68b76471
bachriotmane <bachriotmane@gmail.com>
2024-01-26 00:52:26 +0100

    final navbar

--- rms-front/src/pages/Client-pages/Pages/HomePage/home.page.jsx
+++ rms-front/src/pages/Client-pages/Pages/HomePage/home.page.jsx
@@ -1,11 +1,5 @@
 export default function HomePage() {
     return (<>
         <h1>Home Page1</h1>
-        {/* //!Search bar - button + background  */}
-        {/* //!cards => Categories */}
-        {/* //! */Restaurants}
-        {/* //! Popular */}
-        {/* //! Special For you */}
-
     </>  );
 }
