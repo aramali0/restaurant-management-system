4d44bdcbbc93a0eb8d8e1cdc0ce763a721e8a713
bachriotmane <bachriotmane@gmail.com>
2024-01-26 22:38:34 +0100

    add home page features

--- rms-front/src/components/Client Componenets/restaurant-card/restaurant.card.jsx
+++ rms-front/src/components/Client Componenets/restaurant-card/restaurant.card.jsx
@@ -1,13 +1,20 @@
 import './restaurant.card.style.css'
+import { FaStar } from "react-icons/fa6";
 function RestaurantCard({restaurantName, restaurantDescription , rrstaurantImage,restaurantCoverImage, restaurantRating}) {
     return (<div>
         <div className='rest-card'>
             <div className='cover-image-rest'> <img className='img-cover' src={restaurantCoverImage} alt="cover-image" /> </div>
             <div className='brand-image-rest'> <img className='img-brand' width="100" src={rrstaurantImage} alt='brand-image' /></div>
-            <div className='info'>
+            <div className='info-rest'>
                 <div className='rest-name'>{restaurantName}</div>
-                <div className='rest-desc'>{restaurantDescription}</div>
-                <div className='rest-rat'>{restaurantRating}</div>
+                <div className='rest-desc'>
+                    <p>{restaurantDescription}</p>
+                    <div className='rest-rat'>
+                        <p>{restaurantRating}</p>
+                        <FaStar style={{color : '#FFD60A'}}/>
+                    </div>
+                </div>
+                
             </div>
         </div>
         </div> );
--- rms-front/src/main.jsx
+++ rms-front/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AppClient from './pages/Client-pages/ClientApp.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppClient />
   </React.StrictMode>,
 )
