5ccfbcfa4debf1daff0a3bd01311555c4154fe26
bachriotmane <bachriotmane@gmail.com>
2024-01-27 00:33:47 +0100

    deal link

--- rms-front/src/constants/index.js
+++ rms-front/src/constants/index.js
@@ -1,20 +1,35 @@
 export const sideLinks = [
- {
-   id: "dashBoard",
-   title: "Dashboard",
- },
- {
-   id: "commands",
-   title: "Commands",
- },
- {
-   id: "clients",
-   title: "Clients",
- },
- {
-   id: "meals",
-   title: "Meals",
- },
+  {
+    id: "dashBoard",
+    title: "Dashboard",
+  },
+  {
+    id: "commands",
+    title: "Commands",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+  {
+    id: "meals",
+    title: "Meals",
+  },
 ];
 
-export const BASE_URL = "http://localhost:8080/api/"
+const CATEGORIES = [
+  "FAST_FOOD",
+  "CASUAL",
+  "FAST_CASUAL",
+  "FINE_DINING",
+  "CAFEE_SHOPS",
+  "DRINKS",
+  "BUFFET",
+  "DESSERT",
+  "ICECREAMS",
+  "BREAKFATS",
+];
+
+const BASE_URL = "http://localhost:8080/api/";
+
+export { CATEGORIES, BASE_URL };
