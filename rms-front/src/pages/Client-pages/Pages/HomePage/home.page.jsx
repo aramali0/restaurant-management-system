975741a1e7db4cc1c4dc162a28fc97635f74cfda
bachriotmane <bachriotmane@gmail.com>
2024-01-27 17:50:09 +0100

    add navigation btw pages

--- rms-front/src/pages/Client-pages/Pages/HomePage/home.page.jsx
+++ rms-front/src/pages/Client-pages/Pages/HomePage/home.page.jsx
@@ -10,11 +10,14 @@ const categoriesColors = ["#a7f6b4","#f6c67e", "#e3a1a2", "#c69ee4","#acc9e7"]
 
 const basePathCat = "src/assets/client-assets/categories-images/"
 export default function HomePage() {
-    const [categorySelected, setCategorySelected] = useState(0)
-    const [restaurants, setRstaurants] = useState([])
-    const [restIsLoading, setRestIsLoading] = useState(true)
-    const [mealsLoading, setMealsLoading] = useState(true)
-    const [meals, setMeals] = useState([])
+    const [categorySelected, setCategorySelected] = useState(0);
+    const [restaurants, setRstaurants] = useState([]);
+    const [mealsByCategory, setMealsByCategory]= useState([]);
+    const [restIsLoading, setRestIsLoading] = useState(true);
+    const [mealsLoading, setMealsLoading] = useState(true);
+    const [countPerCategory, setCountPerCategory] = useState({})
+    const [mealsByCatIsLoading, setMealsByCatIsLoading] = useState(true)
+    const [meals, setMeals] = useState([]);
 
     const getRestaurants = ()=>{
         fetch('http://localhost:8080/api/restaurants').then((resp)=>{
@@ -24,6 +27,7 @@ export default function HomePage() {
             })
         })
     }
+
     const getMeals = ()=>{
         fetch('http://localhost:8080/api/articles').then((resp)=>{
             resp.json().then((resp)=>{
@@ -32,9 +36,25 @@ export default function HomePage() {
             })
         })
     }
+    const getMealsByCategory = (category)=>{
+        fetch('http://localhost:8080/api/articles/search/findArticlesBycategorie?categorie='+category).then((resp)=>{
+            resp.json().then((resp)=>{
+                setMealsByCategory(resp._embedded.articles)
+                setCountPerCategory(prevState =>{
+                    let cats = prevState
+                    cats[category] = resp._embedded.articles.length;
+                    return cats;
+                })
+                setMealsByCatIsLoading(false)
+            })
+        })
+    }
     useEffect(()=>{
         getRestaurants();
         getMeals();
+        CATEGORIES.map((cat)=>{
+            getMealsByCategory(cat);
+        })
     },[])
     const handleClick = (eO)=>{
         eO.currentTarget.classList.toggle('active');
@@ -52,8 +72,7 @@ export default function HomePage() {
         return restaurants.map((rest, key)=>{
             return <RestaurantCard 
             key = {key}
-            restaurantName={rest.nomRestaurant} 
-            restaurantDescription={rest.description}
+            restaurant={rest}
             restaurantRating={3.2}
             rrstaurantImage="src/assets/client-assets/restautant-images/rest-brand.jpg"
             restaurantCoverImage="src/assets/client-assets/restautant-images/rest-cover.jpg"
@@ -63,11 +82,10 @@ export default function HomePage() {
     const displayMealsItems = ()=>{
         return meals.map((meal, key)=>{
             return <MealCard 
-            mealName= {meal.name}
-            mealPrice={meal.prix +'$'}
-            mealRating={4.3}
-            mealImage="src/assets/client-assets/restautant-images/meal1.jpg"
-        />;
+            meal={meal}
+            key={key}
+            mealImage="src/assets/client-assets/restautant-images/meal1.jpg">
+            </MealCard>;
         })
     }
     const displaCategiries = ()=>{
@@ -75,16 +93,25 @@ export default function HomePage() {
         return CATEGORIES.map((cat,key)=>{
             index >= categoriesColors.length ? index = 0 : undefined;
             index = index+1
-            
             return <div onClick={()=>handleCategoryClicked(key+1)}>
                     <CategoryCard  
                         onTap={handleClick} 
                         categoryName={cat}
                         color={index<categoriesColors.length && index>=0 ?  categoriesColors[index] : categoriesColors[0]} 
-                        itemCount={3} 
-                        imageUrl={basePathCat + "cat1.png"}  
+                        itemCount={countPerCategory[cat]} 
+                        imageUrl={basePathCat + `${cat}.png`}  
                         />
                 </div>
+        })
+    }
+    const displayMealsByCategoryName = (categpryName)=>{
+        getMealsByCategory(categpryName);
+        return mealsByCategory.map((meal, key)=>{
+            return  <MealCard 
+            key={key}
+            meal={meal}
+            mealImage={`src/assets/client-assets/restautant-images/meal1.jpg`}
+        />;
         })
     }
     return (<>
@@ -95,7 +122,7 @@ export default function HomePage() {
         </div>
         {
             CATEGORIES.map((cat,index)=>{
-                return categorySelected!==0 && categorySelected ===index+1 ? <div>{cat}</div> : undefined
+                return categorySelected!==0 && categorySelected ===index+1 ? <div className="meals-items">{mealsByCatIsLoading ? "Is Loading" :displayMealsByCategoryName(cat)}</div> : undefined
             })
     }
         <h1 className="main-title">Restaurants</h1>
