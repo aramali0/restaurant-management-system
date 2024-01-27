049393ff88aa4183c3c4247e1244aa9e9662106a
ARAMALI Mohammed <mouhammedaramali@gmail.com>
2024-01-27 21:59:07 +0100

    modification in the back add status for commande , add more data add serch function in dao

--- Restaurant-Management-System-Back/src/main/java/RMS/dao/ClientDAO.java
+++ Restaurant-Management-System-Back/src/main/java/RMS/dao/ClientDAO.java
@@ -1,10 +1,20 @@
 package RMS.dao;
 
 import RMS.entity.Client;
+import RMS.entity.Commande;
 import RMS.entity.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @CrossOrigin
 public interface ClientDAO extends JpaRepository<Client, Long> {
+    @Query("SELECT c FROM Client c WHERE c.nomPersonne LIKE %:nomPersonne%")
+    Page<Client> findClientsByNomPersonne(@Param("nomPersonne") String nomPersonne, Pageable pageable);
+
 }
--- Restaurant-Management-System-Back/src/main/java/RMS/dao/CommandeDAO.java
+++ Restaurant-Management-System-Back/src/main/java/RMS/dao/CommandeDAO.java
@@ -1,10 +1,17 @@
 package RMS.dao;
 
+import RMS.entity.Article;
+import RMS.entity.Categorie;
 import RMS.entity.Commande;
 import RMS.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @CrossOrigin
 public interface CommandeDAO extends JpaRepository<Commande, Long> {
+    public List<Commande> findCommandesByClientNomPersonne(@Param("name") String name);
+
 }
