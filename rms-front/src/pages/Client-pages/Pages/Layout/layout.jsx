95f781db43f580b3ab3580bf83c7b940592c9055
bachriotmane <bachriotmane@gmail.com>
2024-01-28 16:46:21 +0100

    deal woth store page

--- rms-front/src/pages/Client-pages/Pages/Layout/layout.jsx
+++ rms-front/src/pages/Client-pages/Pages/Layout/layout.jsx
@@ -3,6 +3,7 @@ import { IoSearch } from "react-icons/io5";
 import  './layout.style.css';
 import ProfileItem from "../../../../components/Client Componenets/prifile item/profile.item";
 import React, { useState } from "react";
+import { CiShoppingCart } from "react-icons/ci";
 
 export const Context = React.createContext();
 
@@ -17,7 +18,6 @@ function LayouClient() {
     <Context.Provider value={[index, incremntIndex]} >
     <>
         <nav>
-            <div>{index}</div>
             <div className="logo"> <img width="60" src="src/assets/client-assets/logo.png" alt="logo" /> </div>
             <div className="search-box">
                 <form onSubmit={handleSubmit}>
@@ -29,6 +29,14 @@ function LayouClient() {
                 <li className={location.pathname === "/home" ? "nav-item active" :"nav-item" }><Link className="nav-link" to = "/home">Home</Link></li>
                 <li className={location.pathname === "/about" ? "nav-item active" :"nav-item" }><Link className="nav-link" to="about">About</Link></li>
                 <li className={location.pathname === "/contact" ? "nav-item active" :"nav-item" }><Link className="nav-link" to="contact">Conatt us</Link></li>
+                <li className={location.pathname === "/cart" ? "nav-item active" :"nav-item"}>
+                    <Link className="nav-link" to="cart">
+                        <div className="cart-size" >
+                            <CiShoppingCart className="cart-icon" />
+                            <span className="cart-count" ><span>{index}</span></span>
+                        </div>
+                    </Link>
+                </li>
                 <li className={location.pathname === "/profile" ? "nav-item active" :"nav-item" }><Link className="nav-link profile" to="profile"><ProfileItem imageLink="src/assets/client-assets/profile.jpg"></ProfileItem></Link></li>
             </ul>
         </nav>
--- rms-front/src/pages/Client-pages/Pages/store-page/store.page.jsx
+++ rms-front/src/pages/Client-pages/Pages/store-page/store.page.jsx
@@ -1,14 +1,77 @@
 import { useLocation } from "react-router-dom";
-
+import './store.page.css'
+import { FaStar } from "react-icons/fa6";
+import CategoryItemText from "../../../../components/Client Componenets/category-item-text/category.item";
+import { useEffect, useState } from "react";
+import MealCard from "../../../../components/Client Componenets/meal-card/meal.card";
 function StorePage() {
     const location = useLocation();
     const restaurant = location.state.restaurant;
+    const imageCover = location.state.imageCover;
+    const imageBrand = location.state.imageBrand
+    const articlesLink = restaurant._links.articles.href;
+    const [articles, setArticles] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [categorieSelected,setCategorieSelected ] = useState('');
+    const handleClick = (categoryTitle)=>{
+        console.log(categoryTitle);
+        setCategorieSelected(categoryTitle)
+    }
+    const getArticles = ()=>{
+        fetch(articlesLink).then((resp)=>{
+            resp.json().then((resp)=>{
+                setArticles(resp._embedded.articles)
+                removeDuplicateCategories(resp._embedded.articles)
+                
+            })
+        })
+    }
+    const removeDuplicateCategories = (articles)=>{
+        var cats = [];
+        articles.map((article)=>{
+            cats.push(article.categorie)
+        })
+        setCategories([ ... new Set(cats)]);
+    }
+    useEffect(()=>{
+        getArticles()
+        
+    }, [])
     return ( <>
-        <h1>Store Page</h1>
-        <div>
-            <h2>Store name :{restaurant.nomRestaurant} </h2>
-            <h2>Store description :{restaurant.description} </h2>
-            <h2>rating : {restaurant.rating}</h2>
+        <div className="container-store">
+            <div className="store-info-container">
+                <div className="cover-image-store">
+                    <img src={imageCover} alt="Cover" />
+                </div>
+                <div className="store-info">
+                    <div className="brand-image">
+                        <img src={imageBrand} alt="" />
+                    </div>
+                    <div className="title-description-rating-store">
+                        <div className="title">
+                            {restaurant.nomRestaurant}
+                        </div>
+                        <div className="rating">
+                            <div className="satar-count">
+                                <div className="stars-rating"><FaStar/></div>
+                                <div className="rating-count">{restaurant.rating}</div>
+                            </div>
+                            <div className="description">{restaurant.description}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="store-caegories">
+                {categories.map((categorie)=>{
+                    return <CategoryItemText onTap={()=>handleClick(categorie)}  categoryTitle={categorie} />
+                })}
+            </div>
+            { categorieSelected!='' ? <div className="store-meals">
+                {articles.map((meal,key)=>{
+                    return meal.categorie ===categorieSelected ?  <MealCard meal={meal} mealImage="src/assets/client-assets/restautant-images/meal1.jpg" />  : undefined ;
+                })}
+                
+            </div> : undefined }
         </div>
     </>);
 }
